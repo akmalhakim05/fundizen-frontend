030e8f98765aa97614eca015eaa923f4c03323ca
akmalhakim05 <akmalhakimjamaluddin05@gmail.com>
2025-11-18 01:49:36 +0800

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Update this when deploying
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = 'https://fundizen.app/api' || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
